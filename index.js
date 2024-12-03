27781529bf4c877c64f912bb19651b7697dbf8bb
Your Name <you@example.com>
2024-12-03 15:31:52 -0500

    test

---
+++ index.js
@@ -0,0 +1,8 @@
+const words=["Hello", "world", "this", "is", "web", "development"]
+const filtered = fruits.filter(function(fruit){
+    if (fruit.substring(0,1) === "C") {
+        return fruit;
+    }
+})
+console.log(filtered)
+

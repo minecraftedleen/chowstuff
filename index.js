bd1be0401acac94a9d70d55bb1c718b6d906de4a
Your Name <you@example.com>
2024-12-11 15:31:56 -0500

    yyyy

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const words=["Hello", "world", "this", "is", "web", "development"]
-const filtered = fruits.filter(function(fruit){
-    if (fruit.substring(0,1) === "C") {
-        return fruit;
-    }
-})
-console.log(filtered)
+async function displayText(){
+    let response = await fetch('poem.txt')
+    let text = await response.text()
+    .then((response) => {
+        console.log("success")
+    })
+    .catch((response) =>{
+        console.log("error")
+    })
+    
+    console.log(text)
+    return text;
+
+    
+}
+
+displayText()
+
+
 

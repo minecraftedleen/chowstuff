5771e652612aa0969ed29b2a17b6715f71f9eda5
edmund <sample@bths.edu>
2024-12-13 00:09:28 -0500

    newthing

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-async function displayText(){
-    let response = await fetch('poem.txt')
-    let text = await response.text()
-    .then((response) => {
-        console.log("success")
-    })
-    .catch((response) =>{
-        console.log("error")
-    })
-    
-    console.log(text)
-    return text;
-
-    
+async function getUsers(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/users/')
+    let users = await response.json()
+    return users
 }
 
-displayText()
+getUsers()
+.then((json)=>{
+    let data=document.getElementById('data')
+    for(let i=0;i<10;i++){
+        let info=JSON.stringify(json[i])
+        info = JSON.parse(info)
+        console.log(info)
+        block = document.createElement("div")
+        block.setAttribute("id", "bl")
+        heading = document.createElement("h2")
+        emAdd = document.createElement("h3")
+        phNum = document.createElement("h3")
+        webSt = document.createElement("h3")
+        heading.innerHTML=info.name + " (" + info.username + ")" 
+        emAdd.innerHTML = "Email me at " + info.email
+        phNum.innerHTML = "Phone number: " + info.phone
+        webSt.innerHTML = "Check out my website! --> " + info.website
+        block.append(heading)
+        block.append(emAdd)
+        block.append(phNum)
+        block.append(webSt)
+        document.body.append(block)
+    }
+    
+})
 
+document.getElementById("welcome").innerHTML="Welcome, " + prompt("Hello, what is your name?") + "!"
+
+
+    
 
 
